22f3240721fb92b1e9bfd1be33b68e2938c61606
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-02-06 18:47:41 -0300

    Create Main.java

---
+++ CodeForces/MakeItWhile/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		int t = teclado.nextInt();
+		teclado.nextLine();
+		while (t < 0 || t > 10000) {
+			t = teclado.nextInt();
+			teclado.nextLine();
+		}
+		
+		for (int i = 0; i < t; i++) {
+			
+			int n = teclado.nextInt();
+			teclado.nextLine();
+			while (n < 0 || n > 10) {
+				n = teclado.nextInt();
+				teclado.nextLine();
+			}
+			
+			String frase = teclado.nextLine();
+			
+			int indice1 = -1;
+			int indice2 = 0;
+			
+			for (int cont = 0; cont < frase.length(); cont++) {
+				
+				if (Character.toString(frase.charAt(cont)).equals("B")) {
+					if (indice1 == -1) {
+						indice1 = cont;
+					} else {
+						indice2 = cont;
+					}
+				}
+				
+			}
+			
+			if (indice2 == 0) {
+				System.out.printf("1\n");
+			} else {
+				System.out.printf("%d\n", indice2 - indice1 + 1);
+			}
+			
+		}
+
+	}	
+
+}
