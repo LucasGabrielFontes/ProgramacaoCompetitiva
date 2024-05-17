2c3b1026459592447035eeef126be5741a68f6a2
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-17 08:14:22 -0300

    Primeiro programa em C++

---
+++ EstudosC++/Aula3/aula3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; // Organização de pastas de bibliotecas
+
+int main () {
+	
+	cout << "Vasco da Gama e nada else" << endl; // Imprime. endl é a quebra de linha. \n também existe
+	// Alternativa: cout << "Mensagem\n";
+	
+	int idade = 18;
+	
+	cout << "Lucas Gabriel tem " << idade << " anos" << endl;
+	
+	system("pause"); // Pausa a execução do programa
+	
+	return 0;
+	
+}
