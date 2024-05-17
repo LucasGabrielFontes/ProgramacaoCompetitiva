fd986586825dcfa7224d1a16fc84d35cbe265c16
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-17 09:43:50 -0300

    Inversão de variáveis

---
+++ EstudosC++/Aula8/aula8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int a = 15;
+	
+	cout << "Valor inicial: " << a << endl;
+	
+	//a *= -1;
+	
+	a = -a;
+	
+	//cout << "Valor invertido: " << -a << endl; // o - inverte o valor momentanamente
+	
+	cout << "Valor invertido> " << a << endl;
+	
+	return 0;
+	
+}
