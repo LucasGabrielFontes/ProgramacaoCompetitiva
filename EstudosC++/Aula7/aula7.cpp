8058d583e7b482b8e75babaa68ef494dc762032a
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-17 09:31:37 -0300

    Incremento e decremento de variáveis

---
+++ EstudosC++/Aula7/aula7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int contador = 0;
+	contador++; // contador += 1; contador = contador + 1;
+	contador--; // contador -= 1; contador = contador - 1;
+	
+	// *=   /=
+	
+	contador++; // Pós incremento - usa o valor e depois incrementa
+	++contador; // Pré decremento - incrementa e depois usa o valor
+	
+	return 0;
+	
+}
