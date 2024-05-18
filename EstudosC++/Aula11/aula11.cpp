68b6803c8e12ce3716885926c2535bd07d3759d1
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-18 08:06:00 -0300

    Operadores lógicos

---
+++ EstudosC++/Aula11/aula11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	// &&
+	// ||
+	// !
+
+	// Já sei disso
+	
+	return 0;
+	
+}
---
+++ EstudosC++/Aula12/aula12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	// Operador ternario
+	// TESTE ? SE O TESTE FOR VERDADEIRO : SE O TESTE FOR FALSO ;
+	
+	int nota = 6;
+	
+	nota >= 7 ? cout << "Aprovado\n" : cout << "Reprovado" ;
+	
+	return 0;
+	
+}
