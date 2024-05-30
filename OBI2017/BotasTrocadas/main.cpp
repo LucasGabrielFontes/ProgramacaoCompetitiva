420d9ebc9dedc72636aa4b45b99a89e88b377e1f
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-30 10:47:33 -0300

    Questão pares programação competitiva

---
+++ OBI2017/BotasTrocadas/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	
+	int N;
+	cin >> N;
+	
+	vector < pair<int, int> > pares(61, {0,0});
+	
+	int tam;
+	char pe;
+	
+	for (int i = 0; i < N; i++) {
+		
+		cin >> tam;
+		cin >> pe;
+		
+		if (pe == 'E') {
+			pares[tam].first++;
+		} else {
+			pares[tam].second++;
+		}
+		
+	}
+	
+	int paresPossiveis = 0;
+	
+	for (int i = 0; i < pares.size(); i++) {
+		paresPossiveis += min(pares[i].first, pares[i].second);
+	}
+	
+	cout << paresPossiveis << endl;
+	
+	return 0;
+	
+}
