9746100da70b2fcceb79bfd5f5a6637a0cff00b5
lucas <lucasgabrielfontesdasilva@gmail.com>
2024-08-04 08:55:33 -0300

    Kana question solution

--- CodeForces/KanaAndDragonQuestGame/main.cpp
+++ CodeForces/KanaAndDragonQuestGame/main.cpp
@@ -7,33 +7,26 @@ int main() {
     int t; cin >> t;
 
     for (int i = 0; i < t; i++) {
-
         int x; // Hit point of the Dragon
         int n; // Maximum number of Void Absorptions
-        int m; // Maximum number of Lightning Strikes
-
+        int m; // Maximum number of Lightning Strikes. Se m == 0, nunca sera possivel ganhar
         cin >> x >> n >> m;
 
-        while (m > 0) {
+        while (n > 0) { // Gasta todas as magias Void Absorptions até que o Hit point entre em um loop onde nao mais diminui
+            int life_after_n = floor(x/2) + 10;
 
-            int magicM = floor(x/2) + 10; // Quanto fica a vida do dragao depois de ser atacado pela magia M
-            int magicN = x-10;
+            if (life_after_n == x || life_after_n > x) break; // Teste do loop feito aqui
 
-            if (magicN < magicM) {
-                x = magicN;
-                break;
-            }
-            if (magicM == x) break; 
-
-            x = magicM;
-            m--;            
-
+            x = life_after_n;
+            n--;
         }
 
-        while(n > 0) {
-            int magicN = x-10;
+        while (m > 0) { // Gasta todas as Lightning Strikes ate que o Hit point seja <= 0 ou as magias acabem
 
-            x = magicN;
+            int life_after_m = x-10;
+
+            x = life_after_m;
+            m--;
 
             if (x <= 0) break;
         }
