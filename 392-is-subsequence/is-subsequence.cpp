f2e41d28d589ed93fce62a8bc4b949561f0ff398
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2025-01-02 19:33:35 -0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (22.55%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -2,24 +2,22 @@ class Solution{
 public:
     bool isSubsequence(string s, string t) {
 
-        if (s == "")
+        if (s == "") // Nada esta contido em qualquer coisa (olha Matematica Discreta ai, meu amigo)
             return true;
 
         char currentLetter = s[0]; // Letra atual da string s que será verificada na string t
         unsigned int count = 0; // Contador para a quantidade de correspondencias e indice para a proxima letra
-        const unsigned int last = s.size(); // Tamanho da string s, que sera tratado como limite para o contador count
+        const unsigned int limit_count = s.size(); // Tamanho da string s, que sera tratado como limite para o contador count
         for (auto letter: t) {
-
-            if (letter == currentLetter) { // Mais uma correspondencia
+            if (letter == currentLetter) // Mais uma correspondencia
                 count++;
-            }
 
-            if (count < last) // O indice da proxima letra ainda e valido
+            if (count < limit_count) // O indice da proxima letra ainda e valido
                 currentLetter = s[count];
-            else if (count == s.size()) // O contador count atingiu seu limete e todas as letras de s foram encontradas em sequencia na string t. Retorna true
+            else if (count == s.size()) // O contador count atingiu seu limite, e todas as letras de s foram encontradas em sequencia na string t. Retorna true
                 return true; 
         }
 
-        return false; 
+        return false; // Nao e subsequencia, rapaz. Retorna false
     }
 };
